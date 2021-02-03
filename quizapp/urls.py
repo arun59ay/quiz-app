b54b78ba85aedbf454fd5980062c07e6d634d2c6
arunyadav59 <arun@zool.in>
2021-02-04 01:47:34 +0530

    first commit changes done deploy..

--- quizapp/urls.py
+++ quizapp/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('' , include('accounts.urls')),
     path('' , include('onlinequiz.urls')),
     path('admin/' ,  admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     
 ]
