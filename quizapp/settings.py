d29ce02abb9031e9661b1162ebd0f3c696e49668
arunyadav59 <arun@zool.in>
2021-02-04 02:38:16 +0530

    first commit changes /

--- quizapp/settings.py
+++ quizapp/settings.py
@@ -124,13 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-if DEBUG:
-   STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static'),
-   ]
-else:
-   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = ''
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+
+
+# STATICFILES_DIRS = (
+
+#     ('assets', os.path.join(PROJECT_DIR, '../static')),
+# )
